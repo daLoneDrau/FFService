ccdef27d678564b0ac8c53ded74280bba9cc2003
daLoneDrau <donaldbrau@yahoo.com>
2017-05-25 13:49:57 -0400

    Thu May 25 13:49:49 EDT 2017

---
+++ src/main/java/com/osrapi/repositories/ff/FFMapLevelRepository.java
@@ -0,0 +1,30 @@
+package com.osrapi.repositories.ff;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.osrapi.models.ff.FFMapLevelEntity;
+
+/**
+ *
+ * @author drau
+ *
+ */
+@Repository
+public interface FFMapLevelRepository
+extends CrudRepository<FFMapLevelEntity, Long> {
+	/**
+	 * Retrieves a list of map levels by their name.
+	 * @param name the name
+	 * @return {@link List}<{@link FFMapLevelEntity}>
+	 */
+	List<FFMapLevelEntity> findByName(String name);
+	/**
+	 * Retrieves a list of map levels by their elevation.
+	 * @param elevation the elevation
+	 * @return {@link List}<{@link FFMapLevelEntity}>
+	 */
+	List<FFMapLevelEntity> findByElevation(Long elevation);
+}
